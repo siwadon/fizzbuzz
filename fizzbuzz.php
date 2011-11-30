0b8f3fe44e00239b509f69f660cafa05e2bf3616
Dew Saosoong <dew@abctech-thailand.com>
2011-11-30 11:35:51 +0700

    added is_it()

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -5,6 +5,26 @@
 * @author Dew Saosoong <dew@abctech-thailand.com>
 */
 class FizzBuzz {
+
+    function is_it($word, $number) {
+        if ($word == 'Fizz') {
+            $divisor = 3;
+        }
+        else if ($word == 'Buzz') {
+            $divisor = 5;
+        }
+        
+        $remainder = $number % $divisor;
+    
+        if ($remainder == 0) {
+            $result = TRUE;
+        }
+        else {
+            $result = FALSE;
+        }
+        
+        return $result;
+    }
         
     function fizzbuzz($number) {
             
@@ -12,8 +32,8 @@ class FizzBuzz {
             throw new InvalidArgumentException("It isn't an integer number");
         }
         
-        $is_fizz     = ($number % 3) == 0 ? TRUE : FALSE;
-        $is_buzz     = ($number % 5) == 0 ? TRUE : FALSE;
+        $is_fizz     = self::is_it('Fizz', $number);
+        $is_buzz     = self::is_it('Buzz', $number);
         $is_fizzbuzz = $is_fizz && $is_buzz;
         
         if ($is_fizzbuzz) {
