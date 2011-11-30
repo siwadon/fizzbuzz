4bf5c7a9cd037b746695d9533a0e9f9e1bb41432
Dew Saosoong <dew@abctech-thailand.com>
2011-11-30 15:03:08 +0700

    more code comment

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -10,6 +10,14 @@ define('Buzz', 5);
 
 class FizzBuzz {
 
+    /**
+     * Calculate and return the remainder from the division of input $number by
+     * 3 if $word is Fizz, or 5 if $word is Buzz
+     * 
+     * @param string $word 'Fizz' or 'Buzz'
+     * @param integer $number input number
+     * @return integer
+     */
     function get_remainder($word, $number) {
         $divisor   = constant($word);
         $remainder = $number % $divisor;
@@ -17,6 +25,14 @@ class FizzBuzz {
         return $remainder;
     }
     
+    /**
+     * Call get_remainder() and then return TRUE if remainder is equal to zero,
+     * or return FALSE if not.
+     * 
+     * @param string $word 'Fizz' or 'Buzz'
+     * @param integer $number input number
+     * @return bool return TRUE if remainder is equal to zero, or FALSE if not.
+     */
     function is_it($word, $number) {        
         $remainder = self::get_remainder($word, $number);
     
@@ -29,7 +45,17 @@ class FizzBuzz {
         
         return $result;
     }
-        
+    
+    /**
+     * Check input $number and return 
+     *   'FizzBuzz' if $number can divide by 3 and 5
+     *   'Fizz'     if $number can divide by 3
+     *   'Buzz'     if $number can divide by 5
+     *   $number    if $number cannot divide by 3 nor 5
+     * 
+     * @param integer $number input number
+     * @return string
+     */    
     function fizzbuzz($number) {
             
         if (!is_numeric($number) || !is_int($number)) {
